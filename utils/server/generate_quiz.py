2c4cb7dff90a76ade4353bbaa7d373fd5d1f0300
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-11-02 16:08:55 -0400

    prompt added

--- utils/server/generate_quiz.py
+++ utils/server/generate_quiz.py
@@ -8,7 +8,8 @@ prompt_to_json_model = genai.GenerativeModel("gemini-1.5-flash",
 regular_model = genai.GenerativeModel("gemini-1.5-flash")
 
 def generate_quiz():
-    prompt = os.getenv("YOUR_PROMPT")
+    # prompt = os.getenv("YOUR_PROMPT")
+    prompt = "Create 10 quizzes with math like ___ + ___ = 14 \n- but make them guess 2 numbers, both less than 10 \n- you can eve use something square somethign is equal your numbers, \n- then return to me a JSON object with the questions and answers as below \nx: int \ny: int \nanswer: int \ninstructions: str \ncombinations: List[Tuple[int, int]] \nReturns: [x, y, answer, instructions, combinations]"
 
     response = prompt_to_json_model.generate_content(prompt)
     array_of_keywords = json.loads(response.text)
